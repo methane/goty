47bf073d8a9ecb55130b86dd8124a64eef4e6c77
INADA Naoki <inada-n@klab.com>
2014-06-09 21:48:30 +0900

    Fix compile error and nick rumbering.

--- goty.go
+++ goty.go
@@ -1,19 +1,22 @@
 package goty
 
 import (
-	"net"
-	"fmt"
-	"os"
 	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
 	"strings"
 )
 
+var numericReplyExp *regexp.Regexp = regexp.MustCompile(`\A(\d\d\d) .*`)
+
 type IRCConn struct {
-	Sock *net.TCPConn
+	Sock        *net.TCPConn
 	Read, Write chan string
 }
 
-func Dial(server, nick string) (*IRCConn, os.Error) {
+func Dial(server, nick string) (*IRCConn, error) {
 	read := make(chan string, 1000)
 	write := make(chan string, 1000)
 	con := &IRCConn{nil, read, write}
@@ -21,59 +24,73 @@ func Dial(server, nick string) (*IRCConn, os.Error) {
 	return con, err
 }
 
-func (con *IRCConn) Connect(server, nick string) os.Error {
-	if raddr, err := net.ResolveTCPAddr(server); err != nil {
+func (con *IRCConn) Connect(server, nick string) error {
+	var raddr *net.TCPAddr
+	var err error
+	if raddr, err = net.ResolveTCPAddr("tcp", server); err != nil {
 		return err
-	} else {
-		if c, err := net.DialTCP("tcp", nil, raddr); err != nil {
-			return err
-		} else {
-			con.Sock = c
-			r := bufio.NewReader(con.Sock)
-			w := bufio.NewWriter(con.Sock)
-
-			go func() {
-				for {
-					if closed(con.Read) {
-						fmt.Fprintf(os.Stderr, "goty: read closed\n")
-						break
-					}
-					if str, err := r.ReadString(byte('\n')); err != nil {
-						fmt.Fprintf(os.Stderr, "goty: read: %s\n", err.String())
-						break
-					} else {
-						if strings.HasPrefix(str, "PING") {
-							con.Write <- "PONG" + str[4:len(str)-2]
-						} else {
-							con.Read <- str[0:len(str)-2]
-						}
-					}
-				}
-			}()
-
-			go func() {
-				for {
-					str := <-con.Write
-					if closed(con.Write) {
-						fmt.Fprintf(os.Stderr, "goty: write closed\n")
-						break
-					}
-					if _, err := w.WriteString(str + "\r\n"); err != nil {
-						fmt.Fprintf(os.Stderr, "goty: write: %s\n", err.String())
-						break
-					}
-					w.Flush()
-				}
-			}()
-
-			con.Write <- "NICK " + nick
-			con.Write <- "USER bot * * :..."
-		}
 	}
+	if con.Sock, err = net.DialTCP("tcp", nil, raddr); err != nil {
+		return err
+	}
+
+	r := bufio.NewReader(con.Sock)
+	w := bufio.NewWriter(con.Sock)
+
+	go func() {
+		nickNum := 1
+		for {
+			var str string
+			if str, err = r.ReadString(byte('\n')); err != nil {
+				fmt.Fprintf(os.Stderr, "goty: read: %s\n", err.Error())
+				break
+			}
+			s := str
+			if strings.HasPrefix(s, ":") {
+				if index := strings.IndexRune(s, ' '); index != -1 {
+					s = s[index+1:]
+				}
+			}
+			if strings.HasPrefix(s, "PING") {
+				con.Write <- "PONG" + str[4:len(str)-2]
+				continue
+			}
+			if m := numericReplyExp.FindStringSubmatch(s); m != nil {
+				switch m[1] {
+				// 433 - ERR_NICKNAMEINUSE
+				// 436 - ERR_NICKCOLLISION
+				// 437 - ERR_UNAVAILRESOURCE
+				case "433", "436", "437":
+					con.Write <- fmt.Sprintf("%s%d", nick, nickNum)
+					nickNum++
+					continue
+				}
+			}
+			con.Read <- str[0 : len(str)-2]
+		}
+	}()
+
+	go func() {
+		for {
+			str, ok := <-con.Write
+			if !ok {
+				fmt.Fprintf(os.Stderr, "goty: write closed\n")
+				break
+			}
+			if _, err := w.WriteString(str + "\r\n"); err != nil {
+				fmt.Fprintf(os.Stderr, "goty: write: %s\n", err.Error())
+				break
+			}
+			w.Flush()
+		}
+	}()
+
+	con.Write <- "NICK " + nick
+	con.Write <- "USER bot * * :..."
 	return nil
 }
 
-func (con *IRCConn) Close() os.Error {
+func (con *IRCConn) Close() error {
 	close(con.Read)
 	close(con.Write)
 	return con.Sock.Close()
